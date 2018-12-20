31422e72073b29eaec8f8f0d46363775f955d7f0
Sahid Sousa <sahid@acception.com.br>
2018-12-20 13:29:41 -0300

    Classe que criptografa e decriptografa

--- src/com/ufra/auditoria/sistemas/Principal.java
+++ src/com/ufra/auditoria/sistemas/Principal.java
@@ -1,14 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.ufra.auditoria.sistemas;
 
-/**
- *
- * @author sahid
- */
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Principal {
-    
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        try {
+            EncriptaDecriptaRSA.setup();
+            System.out.println("Informe um texto: ");
+            String menssagem = entrada.next();
+            
+            byte[] textoCriptografado = EncriptaDecriptaRSA.criptografa(menssagem);
+            String textoPuro = EncriptaDecriptaRSA.decriptografa(textoCriptografado);
+            
+            System.out.println("Menssagem: " + menssagem);
+            System.out.println("Mensagem Criptografada: " + Arrays.toString(textoCriptografado));
+            System.out.println("Mensagem Decriptografada: " + textoPuro);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace(System.err);
+        }
+    }
+
 }
